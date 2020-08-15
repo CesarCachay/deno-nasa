45c3ea1eb5e5b56e35595e073a58828c11c96dbf
CesarCachay <cesarcachay1996@gmail.com>
2020-08-15 12:38:15 -0500

    Add middlewares and send headers on our API

--- mod.ts
+++ mod.ts
@@ -3,6 +3,21 @@ import { Application } from "https://deno.land/x/oak@v5.0.0/mod.ts";
 const app = new Application();
 const PORT = 8000;
 
+// the 2nd parameter next is an async function
+app.use(async (ctx, next) => {
+  await next();
+  const time = ctx.response.headers.get("X-Response-Time");
+  console.log(`${ctx.request.method} ${ctx.request.url}: ${time}`);
+});
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  // This one will execute when the next middleware finished because of the await next
+  await next();
+  const delta = Date.now() - start;
+  ctx.response.headers.set("X-Response-Time", `${delta} ms`);
+});
+
 // ctx or context is an object which contains the current state of the application
 app.use((ctx) => {
   ctx.response.body = `Hello from the server`;

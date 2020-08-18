35965ee4abbad175cee5f32d7f7b619118ff4314
CesarCachay <cesarcachay1996@gmail.com>
2020-08-17 19:36:12 -0500

    Add planets model and route

--- models/planets.tests.ts
+++ models/planets.tests.ts
@@ -1,1 +1,33 @@
-Deno.test({});
+import {
+  assertEquals,
+  assertNotEquals,
+} from "https://deno.land/std/testing/asserts.ts";
+
+Deno.test({
+  name: "example test",
+  ignore: Deno.build.os === "windows",
+  fn() {
+    assertEquals("deno", "deno");
+    assertNotEquals({
+      runtime: "deno",
+    }, {
+      runtime: "node",
+    });
+  },
+});
+
+Deno.test({
+  name: "ops leak",
+  sanitizeOps: false,
+  fn() {
+    setTimeout(() => console.log("wait me for 10 secs"), 10000);
+  },
+});
+
+Deno.test({
+  name: "resource leak",
+  sanitizeResources: false,
+  async fn() {
+    await Deno.open("./models/planets.ts");
+  },
+});
--- models/planets.ts
+++ models/planets.ts
@@ -7,6 +7,19 @@ type Planet = Record<string, string>;
 
 let planets: Array<Planet>;
 
+export const filterHabitablePlanets = (planets: Array<Planet>) => {
+  return planets.filter((planet) => {
+    const planetaryRadius = Number(planet["koi_prad"]);
+    const stellarMass = Number(planet["koi_smass"]);
+    const stellarRadius = Number(planet["koi_srad"]);
+
+    return planet["koi_disposition"] === "CONFIRMED" &&
+      planetaryRadius > 0.5 && planetaryRadius < 1.5 &&
+      stellarMass > 0.78 && stellarMass < 1.04 &&
+      stellarRadius > 0.99 && stellarRadius < 1.01;
+  });
+};
+
 const loadPlanetsData = async () => {
   // We use the function join to build the correct path
   // because the paths in macOS/Linux/Windows are totally different
@@ -22,16 +35,7 @@ const loadPlanetsData = async () => {
   // Is necessary to close after use Deno.open otherwise the program will crash.
   Deno.close(file.rid);
 
-  const planets = (result as Array<Planet>).filter((planet) => {
-    const planetaryRadius = Number(planet["koi_prad"]);
-    const stellarMass = Number(planet["koi_smass"]);
-    const stellarRadius = Number(planet["koi_srad"]);
-
-    return planet["koi_disposition"] === "CONFIRMED" &&
-      planetaryRadius > 0.5 && planetaryRadius < 1.5 &&
-      stellarMass > 0.78 && stellarMass < 1.04 &&
-      stellarRadius > 0.99 && stellarRadius < 1.01;
-  });
+  const planets = filterHabitablePlanets(result as Array<Planet>);
 
   return planets.map((planet) => {
     return pick(planet, [
